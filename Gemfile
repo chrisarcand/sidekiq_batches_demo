b34f0c269143ea490e5d3304dd7e745f83a27ae6
Chris Arcand <chris@chrisarcand.com>
2024-07-13 10:57:19 -0500

    Ditch ostruct

--- Gemfile
+++ Gemfile
@@ -6,6 +6,5 @@ gem "sidekiq-ent", "~> 2.5.3", source: "https://enterprise.contribsys.com"
 
 gem 'redis'
 gem 'dotenv'
-gem 'ostruct'
 gem 'puma'
 gem 'rack-session'
--- enqueue.rb
+++ enqueue.rb
@@ -8,7 +8,8 @@ Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
 end
 
-order = OpenStruct.new(id: 1)
+Order = Struct.new('Order', :id)
+order = Order.new(1)
 overall = Sidekiq::Batch.new
 overall.on(:complete, 'FulfillmentCallbacks#complete', 'oid' => order.id)
 overall.on(:success, 'FulfillmentCallbacks#shipped', 'oid' => order.id)
